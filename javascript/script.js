b70c998140ee3f772d9a8dfdfe418ba9ab8609b7
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2024-08-12 22:58:09 -0300

    atualizei a tela

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+var nome = document.getElementById('nome')
+var labelnome = document.getElementById('labelnome')
+var validnome = false
+
+var idade = document.getElementById('idade')
+var labelidade = document.getElementById('labelidade') 
+
+var peso = document.getElementById('peso')
+var labelpeso = document.getElementById('labelpeso')
+
+
+function corignome() {
+    if(nome.value.length <=4) {
+    labelnome.setAttribute('style', 'color: #EA9049')
+    labelnome.innerHTML = 'Pelo menos 5 carateres'
+    validnome = false
+
+  } else {
+    labelnome.setAttribute('style', 'color: black')
+    labelnome.innerHTML = 'Nome'
+    validnome = true  
+  }     
+} 
+
+function verificar() {
+   alert = ('ola' + nome) 
+}
